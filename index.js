16ce2b3b832ec3042ab8bfcd25e6a96036603498
Zac Willmington <zac.willmington@gmail.com>
2018-09-05 19:09:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // Write your code in this file!
 let customers;
-function scuberGreetingForFeet(amount) {
-    if (amount <= 400) {
+function scuberGreetingForFeet(distance) {
+    if (distance <= 400) {
         customers = "This one is on me!";
+    } else if (distance < 400 && distance < 2500 ) {
+        customers = "I will gladly take your thirty bucks.";
+    } else {
+        customers = "No can do."
     }
     return customers;
 }
